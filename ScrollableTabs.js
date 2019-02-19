d938112867c9e594150f3d3944e3224c57c54de4
Muskein Singh <muskein.singh@flipkart.com>
2019-02-20 03:40:29 +0530

    new version with width fix

--- ScrollableTabs.js
+++ ScrollableTabs.js
@@ -280,12 +280,13 @@ const styles = StyleSheet.create({
         paddingRight: 20,
     },
     container: {
-        height: 50,
+        height: 54,
         borderWidth: 1,
         borderTopWidth: 0,
         borderLeftWidth: 0,
         borderRightWidth: 0,
         borderColor: '#ccc',
+        width: WINDOW_WIDTH
     },
     tabs: {
         flexDirection: 'row',
